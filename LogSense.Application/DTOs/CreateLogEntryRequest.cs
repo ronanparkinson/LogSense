1f32b01e13871336afa2b5b684523af6f602f425
ronanparkinson <parkinson95@hotmail.co.uk>
2026-07-25 15:32:22 +0100

    Add log ingestion endpoint

---
+++ LogSense.Application/DTOs/CreateLogEntryRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogSense.Application.DTOs
+{
+    public class CreateLogEntryRequest
+    {
+        public string Level { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        public string Source { get; set; } = string.Empty;
+
+        public string? Exception { get; set; }
+    }
+}
