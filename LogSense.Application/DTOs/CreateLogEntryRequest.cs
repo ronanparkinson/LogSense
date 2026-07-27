36c843bc3ec8381133e3c856136a2a7aec647c1e
ronanparkinson <parkinson95@hotmail.co.uk>
2026-07-27 11:56:00 +0100

    Add request validation using data annotations

--- LogSense.Application/DTOs/CreateLogEntryRequest.cs
+++ LogSense.Application/DTOs/CreateLogEntryRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace LogSense.Application.DTOs
 {
     public class CreateLogEntryRequest
     {
+        [Required]
         public string Level { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(10)]
         public string Message { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(20)]
         public string Source { get; set; } = string.Empty;
 
         public string? Exception { get; set; }
